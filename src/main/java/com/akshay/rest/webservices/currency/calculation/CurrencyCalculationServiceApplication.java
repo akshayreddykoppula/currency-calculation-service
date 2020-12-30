bb4e4ee4eb9164ad5b7a24647c04965b47fda4c7
Koppula Akshay Reddy <akshay@koppulas-air.home>
2020-12-30 15:28:48 -0600

    Using Feign REST client for service invocation

--- src/main/java/com/akshay/rest/webservices/currency/calculation/CurrencyCalculationServiceApplication.java
+++ src/main/java/com/akshay/rest/webservices/currency/calculation/CurrencyCalculationServiceApplication.java
@@ -2,10 +2,12 @@ package com.akshay.rest.webservices.currency.calculation;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
+@EnableFeignClients("com.akshay.rest.webservices.currency.calculation")
 public class CurrencyCalculationServiceApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/akshay/rest/webservices/currency/calculation/controller/CurrencyCalculationController.java
+++ src/main/java/com/akshay/rest/webservices/currency/calculation/controller/CurrencyCalculationController.java
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
+import com.akshay.rest.webservices.currency.calculation.CurrencyExchangeServiceProxy;
 import com.akshay.rest.webservices.currency.calculation.entity.CurrencyCalculationBean;
 
 @RestController
@@ -22,6 +23,9 @@ public class CurrencyCalculationController {
 	
 	@Autowired
 	private RestTemplate restTemplate;
+	
+	@Autowired
+	private CurrencyExchangeServiceProxy currencyExchangeServiceProxy;
 
 	@GetMapping("currency-calculation/from/{from}/to/{to}/quantity/{quantity}")
 	public CurrencyCalculationBean calculateCurrency(@PathVariable String from, @PathVariable String to, @PathVariable BigDecimal quantity) {
@@ -30,6 +34,15 @@ public class CurrencyCalculationController {
 		uriVariables.put("to", to);
 		ResponseEntity<CurrencyCalculationBean> responseEntity = restTemplate.getForEntity("http://localhost:8000/currency-exchange/from/{from}/to/{to}", CurrencyCalculationBean.class, uriVariables);
 		CurrencyCalculationBean currentCalculationBean = responseEntity.getBody();
+		currentCalculationBean.setQuantity(quantity);
+		currentCalculationBean.setTotalCalculatedAmount(quantity.multiply(currentCalculationBean.getConversionMultiple()));
+		currentCalculationBean.setPort(Integer.parseInt(environment.getProperty("server.port")));
+		return currentCalculationBean;
+	}
+	
+	@GetMapping("currency-calculation-feign/from/{from}/to/{to}/quantity/{quantity}")
+	public CurrencyCalculationBean calculateCurrencyFeign(@PathVariable String from, @PathVariable String to, @PathVariable BigDecimal quantity) {
+		CurrencyCalculationBean currentCalculationBean = currencyExchangeServiceProxy.retrieveCurrencyExchangeValue(from, to);
 		currentCalculationBean.setQuantity(quantity);
 		currentCalculationBean.setTotalCalculatedAmount(quantity.multiply(currentCalculationBean.getConversionMultiple()));
 		currentCalculationBean.setPort(Integer.parseInt(environment.getProperty("server.port")));
